feb38ba1dc59aff1b103bfffd5aae1c95b45eeb9
zyrell <raniasabirhussain@gmail.com>
2025-12-31 02:14:10 +0800

    Create string_concatenation.py
    String concatenation is used to join strings together using the + operator.
    It is one way to display variables inside a string.
    
    When a variable is not a string (for example, an integer like grade), we use str() to convert it into a string so it can be joined with other strings.

---
+++ string_concatenation.py
@@ -0,0 +1,13 @@
+# String Concatenation Example (Fictional Character)
+
+name = "Alex"
+grade = 13
+dream = "to become a software engineer"
+favourite_colour = "green"
+
+print(
+    "My name is " + name +
+    ", I study in grade " + str(grade) +
+    ", my dream is " + dream +
+    ", and my favourite colour is " + favourite_colour
+)
