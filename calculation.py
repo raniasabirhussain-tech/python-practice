f7e0c09b2bcd968f744004e6140fd8903b776569
raniasabirhussain-tech <raniasabirhussain@gmail.com>
2026-02-04 01:38:27 +0800

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,11 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+result_with_brackets=num1+num2/num3
+result_without_brackets=(num1*num2)/num3
+print(f"result with brackets: {result_with_brackets}")
+print(f"result without brackets: {result_without_brackets}")
+print(f"the multiplication of two numbers will be {num1*num2}")
+print(f"the addition of two numbers will be {num1+num2}")
+print(f"the division of two numbers is{num1/num3}")
+print(f"the bodmas of all the number can be {num1*num2+num3/num1}")
